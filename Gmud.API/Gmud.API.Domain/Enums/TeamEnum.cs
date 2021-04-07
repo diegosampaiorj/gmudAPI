0ddbe22b5d74c331c2e4fec177bc9aa1dad4c750
Diego Sampaio da Costa <ext.diego.costa@farfetch.com>
2021-04-07 16:33:52 -0300

    subida de enums

---
+++ Gmud.API/Gmud.API.Domain/Enums/TeamEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gmud.API.Domain.Enums
+{
+    public enum TeamEnum
+    {
+        Sustentacao = 0,
+        Massificados = 1,
+        Projetos = 2,
+        Infraestrutura = 3,
+
+    }
+}
