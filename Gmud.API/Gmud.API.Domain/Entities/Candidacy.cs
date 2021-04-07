3158e122b6534b20ceacde6f249ccb41eec1f08d
Diego Sampaio da Costa <ext.diego.costa@farfetch.com>
2021-04-07 09:45:08 -0300

    mudança da estrutura basica

---
+++ Gmud.API/Gmud.API.Domain/Entities/Candidacy.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gmud.API.Domain.Entities
+{
+    public class Candidacy
+    {
+        public int MyProperty { get; set; }
+    }
+}
